926f0551464585521495f9626b9e8ef8e0986556
Vitalii Latysh <vitalii.latysh@globallogic.com>
2018-01-16 18:44:55 +0200

    Overridden doEquals,doHashCode for Entity

--- model/src/main/java/com/globallogic/dc/model/Entity.java
+++ model/src/main/java/com/globallogic/dc/model/Entity.java
@@ -1,5 +1,19 @@
 package com.globallogic.dc.model;
 
-public abstract class Entity extends Aggregate implements Identifiable {
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
+public abstract class Entity <T> extends Aggregate implements Identifiable <T> {
+
+    @Override
+    protected void doEquals(final EqualsBuilder equalsBuilder, final Object obj){
+        equalsBuilder
+                .append(getIdentifier(), getIdentifier());
+    }
+
+    @Override
+    protected  void doHashCode(final HashCodeBuilder hashCodeBuilder){
+        hashCodeBuilder
+                .append(getIdentifier());
+    }
 }
