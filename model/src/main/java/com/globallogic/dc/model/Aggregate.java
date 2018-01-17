0050032cf1e1b716b2879c72d49c8d9e6a604be9
Vitalii Latysh <vitalii.latysh@globallogic.com>
2018-01-17 11:52:53 +0200

    Updated from Entity to Aggregate

--- model/src/main/java/com/globallogic/dc/model/Aggregate.java
+++ model/src/main/java/com/globallogic/dc/model/Aggregate.java
@@ -5,7 +5,7 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 public abstract class Aggregate {
 
-    protected abstract void doEquals(final EqualsBuilder equalsBuilder, final Entity obj);
+    protected abstract void doEquals(final EqualsBuilder equalsBuilder, final Aggregate obj);
 
     protected abstract void doHashCode(final HashCodeBuilder hashCodeBuilder);
 
@@ -18,15 +18,20 @@ public abstract class Aggregate {
         } else if (obj.getClass() != getClass()) {
             return false;
         }
+
         final EqualsBuilder equalsBuilder = new EqualsBuilder();
+
         doEquals(equalsBuilder, (Entity) obj);
+
         return equalsBuilder.isEquals();
     }
 
     @Override
     public int hashCode() {
         final HashCodeBuilder hashCodeBuilder = new HashCodeBuilder(17, 37);
+
         doHashCode(hashCodeBuilder);
+
         return hashCodeBuilder.toHashCode();
     }
 }
