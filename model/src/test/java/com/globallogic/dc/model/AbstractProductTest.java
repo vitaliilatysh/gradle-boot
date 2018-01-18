21328e6eeb31d831292c8b0e09fa4ccfde310719
Vitalii Latysh <vitalii.latysh@globallogic.com>
2018-01-18 11:06:53 +0200

    Remove getIdentifier()

--- model/src/test/java/com/globallogic/dc/model/AbstractProductTest.java
+++ model/src/test/java/com/globallogic/dc/model/AbstractProductTest.java
@@ -7,13 +7,8 @@ import static org.junit.Assert.assertEquals;
 public class AbstractProductTest {
 
     @Test
-    public void testGetIdentifier(){
-        AbstractProduct abstractProduct = new AbstractProduct("1", "Title", "Desc") {
-            @Override
-            public String getIdentifier() {
-                return super.getIdentifier();
-            }
-        };
+    public void testGetIdentifier() {
+        AbstractProduct abstractProduct = new AbstractProduct("1", "Title", "Desc") {};
 
         assertEquals("1", abstractProduct.getIdentifier());
     }
