19286cc89c41eb3dced766ac3c03cd7267c7bd00
Vitalii Latysh <vitalii.latysh@globallogic.com>
2018-01-18 18:28:15 +0200

    Updated Range tests with buildRange method

--- model/src/test/java/com/globallogic/dc/model/RangeTest.java
+++ model/src/test/java/com/globallogic/dc/model/RangeTest.java
@@ -1,7 +1,6 @@
 package com.globallogic.dc.model;
 
 import org.apache.commons.lang3.builder.HashCodeBuilder;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -11,68 +10,66 @@ import static org.junit.Assert.*;
 
 public class RangeTest {
 
-    private Range target, range;
-
-    @Before
-    public void setup() {
-        target = new Range("1", "Title", "Desc");
-        range = new Range("1", "Title", "Desc");
+    @Test
+    public void testHasSubChapters() {
+        assertTrue(buildRange(true, false, false).hasSubChapters());
     }
 
     @Test
-    public void testHasSubChapters() {
-        assertNull(target.getSubChapters());
+    public void testHasSubChapters_Empty() {
+        final Range target = buildRange(false, false, false);
         assertFalse(target.hasSubChapters());
 
         target.addSubChapters(new ArrayList<>());
 
         assertFalse(target.hasSubChapters());
-
-        target.addSubChapter(new SubChapter("1", "Title", "Desc"));
-
-        assertNotNull(target.getSubChapters());
-        assertTrue(target.hasSubChapters());
     }
 
     @Test
     public void testHasItems() {
-        assertNull(target.getItems());
+        assertTrue(buildRange(false, true, false).hasItems());
+    }
+
+    @Test
+    public void testHasItems_Empty() {
+        final Range target = buildRange(false, false, false);
+
         assertFalse(target.hasItems());
 
         target.addItems(new ArrayList<>());
 
         assertFalse(target.hasItems());
-
-        target.addItem(new Item("1", "Title", "Desc"));
-
-        assertNotNull(target.getItems());
-        assertTrue(target.hasItems());
     }
 
     @Test
     public void testHasSections() {
-        assertNull(target.getSections());
+        assertTrue(buildRange(false, false, true).hasSections());
+    }
+
+    @Test
+    public void testHasSections_Empty() {
+        final Range target = buildRange(false, false, false);
+
         assertFalse(target.hasSections());
 
         target.addSections(new ArrayList<>());
 
         assertFalse(target.hasSections());
-
-        target.addSection(new Section("1", "Title", "Desc"));
-
-        assertNotNull(target.getSections());
-        assertTrue(target.hasSections());
     }
 
     @Test
-    public void testAddItems() {
+    public void testAddItem() {
+        final Range target = buildRange(false, false, false);
+
         target.addItem(new Item("1", "Title", "Desc"));
 
         assertEquals(1, target.getItems().size());
     }
 
     @Test
-    public void testAddItem() {
+    public void testAddItems() {
+        final Range target = buildRange(false, false, false);
+
         target.addItems(Arrays.asList(
                 new Item("2", "2", "2"),
                 new Item("3", "3", "3")));
@@ -81,14 +78,18 @@ public class RangeTest {
     }
 
     @Test
-    public void testAddSubChapters() {
+    public void testAddSubChapter() {
+        final Range target = buildRange(false, false, false);
+
         target.addSubChapter(new SubChapter("1", "Title", "Desc"));
 
         assertEquals(1, target.getSubChapters().size());
     }
 
     @Test
-    public void testAddSubChapter() {
+    public void testAddSubChapters() {
+        final Range target = buildRange(false, false, false);
+
         target.addSubChapters(Arrays.asList(
                 new SubChapter("2", "2", "2"),
                 new SubChapter("3", "3", "3")));
@@ -97,14 +98,18 @@ public class RangeTest {
     }
 
     @Test
-    public void testAddSections() {
+    public void testAddSection() {
+        final Range target = buildRange(false, false, false);
+
         target.addSection(new Section("1", "Title", "Desc"));
 
         assertEquals(1, target.getSections().size());
     }
 
     @Test
-    public void testAddSection() {
+    public void testAddSections() {
+        final Range target = buildRange(false, false, false);
+
         target.addSections(Arrays.asList(
                 new Section("2", "2", "2"),
                 new Section("3", "3", "3")));
@@ -113,151 +118,203 @@ public class RangeTest {
     }
 
     @Test
-    public void testEqualsKey() {
+    public void testEquals() {
+        final Range target = buildRange(true, true, true);
+        final Range anotherRange = buildRange(true, true, true);
 
-        range.setKey("2");
-
-        assertFalse(range.equals(target));
-
-        range.setKey("1");
-
-        assertTrue(range.equals(target));
+        assertTrue(target.equals(anotherRange));
     }
 
     @Test
-    public void testEqualsTitle() {
-        range.setTitle("NewTitle");
+    public void testEquals_NotEqual_Key() {
+        final Range target = buildRange("2", "Title", "Desc", true, true, true);
+        final Range anotherRange = buildRange("1", "Title", "Desc", true, true, true);
 
-        assertFalse(range.equals(target));
-
-        range.setTitle("Title");
-
-        assertTrue(range.equals(target));
+        assertFalse(target.equals(anotherRange));
     }
 
     @Test
-    public void testEqualsDesc() {
-        range.setDescription("NewDesc");
+    public void testEquals_NotEqual_Title() {
+        final Range target = buildRange("1", "NewTitle", "Desc", true, true, true);
+        final Range anotherRange = buildRange("1", "Title", "Desc", true, true, true);
 
-        assertFalse(range.equals(target));
-
-        range.setDescription("Desc");
-
-        assertTrue(range.equals(target));
+        assertFalse(target.equals(anotherRange));
     }
 
     @Test
-    public void testEqualsEmptySubChaptersList() {
-        range.addSubChapters(new ArrayList<>());
+    public void testEquals_NotEqual_Desc() {
+        final Range target = buildRange("1", "Title", "NewDesc", true, true, true);
+        final Range anotherRange = buildRange("1", "Title", "Desc", true, true, true);
 
-        assertFalse(range.equals(target));
+        assertFalse(target.equals(anotherRange));
+    }
+
+    @Test
+    public void testEquals_NotEqual_SubChaptersListNotSet() {
+        final Range target = buildRange(false, true, true);
+        final Range anotherRange = buildRange(true, true, true);
+
+        assertFalse(target.equals(anotherRange));
+    }
+
+    @Test
+    public void testEquals_NotEqual_SubChaptersListEmpty() {
+        final Range target = buildRange(false, true, true);
+        final Range anotherRange = buildRange(true, true, true);
 
         target.addSubChapters(new ArrayList<>());
 
-        assertTrue(range.equals(target));
+        assertFalse(target.equals(anotherRange));
     }
 
     @Test
-    public void testEqualsSameSubChaptersInsideList() {
-        range.addSubChapter(new SubChapter("1", "Title", "Desc"));
-
-        assertFalse(range.equals(target));
+    public void testEquals_NotEqual_SubChaptersListContainsDiffNumberOfSubChapters() {
+        final Range target = buildRange(false, true, true);
+        final Range anotherRange = buildRange(true, true, true);
 
         target.addSubChapter(new SubChapter("1", "Title", "Desc"));
 
-        assertTrue(range.equals(target));
+        assertFalse(target.equals(anotherRange));
     }
 
     @Test
-    public void testEqualsDiffSubChaptersInsideList() {
-        range.addSubChapter(new SubChapter("1", "Title", "Desc"));
+    public void testEquals_NotEqual_SubChaptersListContainsTheSameNumberOfSubChaptersButDiff() {
+        final Range target = buildRange(false, true, true);
+        final Range anotherRange = buildRange(true, true, true);
 
-        assertFalse(range.equals(target));
+        target.addSubChapters(Arrays.asList(
+                new SubChapter("1", "Title", "Desc"),
+                new SubChapter("1", "Title", "Desc")));
 
-        target.addSubChapter(new SubChapter("2", "Title", "Desc"));
-
-        assertFalse(range.equals(target));
+        assertFalse(target.equals(anotherRange));
     }
 
     @Test
-    public void testEqualsEmptyItemsList() {
-        range.addItems(new ArrayList<>());
+    public void testEquals_NotEqual_ItemsListNotSet() {
+        final Range target = buildRange(true, false, true);
+        final Range anotherRange = buildRange(true, true, true);
 
-        assertFalse(range.equals(target));
+        assertFalse(target.equals(anotherRange));
+    }
+
+    @Test
+    public void testEquals_NotEqual_ItemsListEmpty() {
+        final Range target = buildRange(true, false, true);
+        final Range anotherRange = buildRange(true, true, true);
 
         target.addItems(new ArrayList<>());
 
-        assertTrue(range.equals(target));
+        assertFalse(target.equals(anotherRange));
     }
 
     @Test
-    public void testEqualsSameItemsInsideList() {
-        range.addItem(new Item("1", "Title", "Desc"));
-
-        assertFalse(range.equals(target));
+    public void testEquals_NotEqual_ItemsListContainsDiffNumberOfItems() {
+        final Range target = buildRange(true, false, true);
+        final Range anotherRange = buildRange(true, true, true);
 
         target.addItem(new Item("1", "Title", "Desc"));
 
-        assertTrue(range.equals(target));
+        assertFalse(target.equals(anotherRange));
     }
 
     @Test
-    public void testEqualsDiffItemsInsideList() {
-        range.addItem(new Item("1", "Title", "Desc"));
+    public void testEquals_NotEqual_ItemsListContainsTheSameNumberOfItemsButDiff() {
+        final Range target = buildRange(true, false, true);
+        final Range anotherRange = buildRange(true, true, true);
 
-        assertFalse(range.equals(target));
+        target.addItems(Arrays.asList(
+                new Item("1", "Title", "Desc"),
+                new Item("1", "Title", "Desc")));
 
-        target.addItem(new Item("2", "Title", "Desc"));
-
-        assertFalse(range.equals(target));
+        assertFalse(target.equals(anotherRange));
     }
 
     @Test
-    public void testEqualsEmptySectionsList() {
-        range.addSections(new ArrayList<>());
+    public void testEquals_NotEqual_SectionsListNotSet() {
+        final Range target = buildRange(true, true, false);
+        final Range anotherRange = buildRange(true, true, true);
 
-        assertFalse(range.equals(target));
+        assertFalse(target.equals(anotherRange));
+    }
+
+    @Test
+    public void testEquals_NotEqual_SectionsListEmpty() {
+        final Range target = buildRange(true, true, false);
+        final Range anotherRange = buildRange(true, true, true);
 
         target.addSections(new ArrayList<>());
 
-        assertTrue(range.equals(target));
+        assertFalse(target.equals(anotherRange));
     }
 
     @Test
-    public void testEqualsSameSectionsInsideList() {
-        range.addSection(new Section("1", "Title", "Desc"));
-
-        assertFalse(range.equals(target));
+    public void testEquals_NotEqual_SectionsListContainsDiffNumberOfSections() {
+        final Range target = buildRange(true, true, false);
+        final Range anotherRange = buildRange(true, true, true);
 
         target.addSection(new Section("1", "Title", "Desc"));
 
-        assertTrue(range.equals(target));
+        assertFalse(target.equals(anotherRange));
     }
 
     @Test
-    public void testEqualsDiffSectionsInsideList()
-    {
-        range.addSection(new Section("1", "Title", "Desc"));
+    public void testEquals_NotEqual_SectionsListContainsSameNumberOfSectionsButDiff() {
+        final Range target = buildRange(true, true, false);
+        final Range anotherRange = buildRange(true, true, true);
 
-        assertFalse(range.equals(target));
+        target.addSections(Arrays.asList(
+                new Section("1", "Title", "Desc"),
+                new Section("1", "Title", "Desc")));
 
-        target.addSection(new Section("2", "Title", "Desc"));
-
-        assertFalse(range.equals(target));
+        assertFalse(target.equals(anotherRange));
     }
 
     @Test
     public void testHashCode() {
+        final Range target = buildRange(true, true, true);
         final HashCodeBuilder hashCodeBuilder = new HashCodeBuilder();
 
         hashCodeBuilder
                 .append(target.getKey())
                 .append(target.getTitle())
                 .append(target.getDescription())
-                .append(target.getSections())
+                .append(target.getSubChapters())
                 .append(target.getItems())
-                .append(target.getSubChapters());
+                .append(target.getSections());
 
         assertEquals(target.hashCode(), hashCodeBuilder.toHashCode());
+    }
+
+    private Range buildRange(final boolean fillSubChapters, final boolean fillItems, final boolean fillSections) {
+        return this.buildRange("1", "Title", "Desc", fillSubChapters, fillItems, fillSections);
+    }
+
+    private Range buildRange(
+            final String id,
+            final String title,
+            final String description,
+            final boolean fillSubChapters,
+            final boolean fillItems,
+            final boolean fillSections) {
+        final Range result = new Range(id, title, description);
+
+        if (fillSubChapters) {
+            result.addSubChapters(Arrays.asList(
+                    new SubChapter("1", "Title", "Desc"),
+                    new SubChapter("2", "Title", "Desc")));
+        }
+
+        if (fillItems) {
+            result.addItems(Arrays.asList(
+                    new Item("1", "Title", "Desc"),
+                    new Item("2", "Title", "Desc")));
+        }
+
+        if (fillSections) {
+            result.addSections(Arrays.asList(
+                    new Section("1", "Title", "Desc"),
+                    new Section("2", "Title", "Desc")));
+        }
+        return result;
     }
 }
