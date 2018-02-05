ff79a58154bfe4dff7f579fb2e1d6373a2d29b59
Vitalii Latysh <vitalii.latysh@globallogic.com>
2018-02-05 15:28:16 +0200

    Added services module, interface ChapterService

---
+++ services/src/main/java/com/globallogic/dc/services/ChapterService.java
@@ -0,0 +1,12 @@
+package com.globallogic.dc.services;
+
+import com.globallogic.dc.model.Chapter;
+
+import java.util.List;
+
+public interface ChapterService {
+
+    List<Chapter> getChapters();
+
+    Chapter getChapterById(String key);
+}
