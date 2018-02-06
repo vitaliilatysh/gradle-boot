452acb81cfcc66cee8848b1daee38d58c9eb2591
Vitalii Latysh <vitalii.latysh@globallogic.com>
2018-02-06 16:48:00 +0200

    getChapters() used readFile()

--- dao/src/main/java/com/globallogic/dc/dao/ChapterDaoImpl.java
+++ dao/src/main/java/com/globallogic/dc/dao/ChapterDaoImpl.java
@@ -3,11 +3,16 @@ package com.globallogic.dc.dao;
 import com.globallogic.dc.connector.FileSystemConnector;
 import com.globallogic.dc.model.Chapter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ChapterDaoImpl implements ChapterDao {
 
     private static volatile ChapterDaoImpl instance = null;
+    private String fileName = "chapters.csv";
+    private Chapter chapter = new Chapter();
+    private List<Chapter> chapters = new ArrayList<>();
+
 
     public static ChapterDaoImpl getInstance() {
         if (instance == null) {
@@ -22,7 +27,22 @@ public class ChapterDaoImpl implements ChapterDao {
 
     @Override
     public List<Chapter> getChapters() {
-        return FileSystemConnector.getInstance().getChapters();
+        List<String> rows = new FileSystemConnector().readFile(fileName);
+        rows.remove(0);
+        for (String row : rows) {
+            String[] chapterElements = row.split(",");
+
+            String key = chapterElements[0];
+            String title = chapterElements[1];
+            String description = chapterElements[2];
+
+            chapter.setKey(key);
+            chapter.setTitle(title);
+            chapter.setDescription(description);
+
+            chapters.add(chapter);
+        }
+        return chapters;
     }
 
     @Override
