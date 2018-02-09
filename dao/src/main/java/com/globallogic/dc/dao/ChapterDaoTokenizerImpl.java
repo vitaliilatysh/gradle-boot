510174454fcad3dece219004bada185589ab4de2
Vitalii Latysh <vitalii.latysh@globallogic.com>
2018-02-09 14:22:22 +0200

    Refactored tokenizer implementation

--- dao/src/main/java/com/globallogic/dc/dao/ChapterDaoTokenizerImpl.java
+++ dao/src/main/java/com/globallogic/dc/dao/ChapterDaoTokenizerImpl.java
@@ -1,18 +1,14 @@
 package com.globallogic.dc.dao;
 
-import com.globallogic.dc.connector.FileSystemConnectorImpl;
 import com.globallogic.dc.model.Chapter;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class ChapterDaoTokenizerImpl extends AbstractFileSystemDAO<Chapter> implements ProductsDao<Chapter> {
 
-    private static volatile ChapterDaoTokenizerImpl instance = null;
     private static final String FILE_NAME = "chapters.csv";
-    private Chapter chapter = new Chapter();
-    private List<Chapter> chapters = new ArrayList<>();
+    private static volatile ChapterDaoTokenizerImpl instance = null;
 
     private ChapterDaoTokenizerImpl() {
     }
@@ -30,45 +26,19 @@ public class ChapterDaoTokenizerImpl extends AbstractFileSystemDAO<Chapter> impl
 
     @Override
     public List<Chapter> getAll() {
-        final List<String> rows = FileSystemConnectorImpl.getInstance().readFile(getFileName());
-        for (String row : rows) {
-            final StringTokenizer stringTokenizer = new StringTokenizer(row, ",", false);
-            while (stringTokenizer.hasMoreTokens()) {
-                final String key = stringTokenizer.nextToken();
-                final String title = stringTokenizer.nextToken();
-                final String description = stringTokenizer.nextToken();
-                chapters.add(new Chapter(key, title, description));
-            }
-        }
-        return chapters;
+        return super.getAll();
     }
 
     @Override
-    public Chapter getById(final String chapterKey) {
-        final List<String> rows = FileSystemConnectorImpl.getInstance().readFile(getFileName());
-        for (String row : rows) {
-            final StringTokenizer stringTokenizer = new StringTokenizer(row, ",", false);
-            while (stringTokenizer.hasMoreTokens()) {
-                String key = stringTokenizer.nextToken();
-                if (key.equals(chapterKey)) {
-                    final String title = stringTokenizer.nextToken();
-                    final String description = stringTokenizer.nextToken();
-                    chapter = new Chapter(key, title, description);
-                    break;
-                } else {
-                    break;
-                }
-            }
-            if (chapter.getIdentifier().equals(chapterKey)) {
-                break;
-            }
-        }
-        return chapter;
+    public Chapter getById(final String id) {
+        return super.getById(id);
     }
 
     @Override
     protected Chapter fromDto(final String dto) {
-        return null;
+        final StringTokenizer stringTokenizer = new StringTokenizer(dto, ",", false);
+
+        return new Chapter(stringTokenizer.nextToken(), stringTokenizer.nextToken(), stringTokenizer.nextToken());
     }
 
     @Override
