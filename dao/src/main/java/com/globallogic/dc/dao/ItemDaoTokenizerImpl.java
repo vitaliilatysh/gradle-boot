a26933d324829421d3cb9b3620e4425996c45180
Vitalii Latysh <vitalii.latysh@globallogic.com>
2018-02-09 14:26:02 +0200

    Added tokenizer implementation for other entities

--- dao/src/main/java/com/globallogic/dc/dao/ItemDaoTokenizerImpl.java
+++ dao/src/main/java/com/globallogic/dc/dao/ItemDaoTokenizerImpl.java
@@ -1,4 +1,48 @@
 package com.globallogic.dc.dao;
 
-public class ItemDaoTokenizerImpl {
+import com.globallogic.dc.model.Item;
+
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class ItemDaoTokenizerImpl extends AbstractFileSystemDAO<Item> implements ProductsDao<Item> {
+
+    private static final String FILE_NAME = "items.csv";
+    private static volatile ItemDaoTokenizerImpl instance = null;
+
+    private ItemDaoTokenizerImpl() {
+    }
+
+    public static ItemDaoTokenizerImpl getInstance() {
+        if (instance == null) {
+            synchronized (ItemDaoTokenizerImpl.class) {
+                if (instance == null) {
+                    instance = new ItemDaoTokenizerImpl();
+                }
+            }
+        }
+        return instance;
+    }
+
+    @Override
+    public List<Item> getAll() {
+        return super.getAll();
+    }
+
+    @Override
+    public Item getById(final String id) {
+        return super.getById(id);
+    }
+
+    @Override
+    protected Item fromDto(final String dto) {
+        final StringTokenizer stringTokenizer = new StringTokenizer(dto, ",", false);
+
+        return new Item(stringTokenizer.nextToken(), stringTokenizer.nextToken(), stringTokenizer.nextToken());
+    }
+
+    @Override
+    protected String getFileName() {
+        return FILE_NAME;
+    }
 }
