d54e5f51a1f8a55606f1c4813981a3c76c1c051e
Vitalii Latysh <vitalii.latysh@globallogic.com>
2018-02-09 15:59:08 +0200

    Added indexOf implementation for other entities

--- dao/src/main/java/com/globallogic/dc/dao/SubChapterDaoIndexOfImpl.java
+++ dao/src/main/java/com/globallogic/dc/dao/SubChapterDaoIndexOfImpl.java
@@ -1,4 +1,61 @@
 package com.globallogic.dc.dao;
 
-public class SubChapterDaoIndexOfImpl {
+import com.globallogic.dc.model.SubChapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubChapterDaoIndexOfImpl extends AbstractFileSystemDAO<SubChapter> implements ProductsDao<SubChapter> {
+
+    private static final String FILE_NAME = "subchapters.csv";
+    private static volatile SubChapterDaoIndexOfImpl instance = null;
+
+    private SubChapterDaoIndexOfImpl() {
+    }
+
+    public static SubChapterDaoIndexOfImpl getInstance() {
+        if (instance == null) {
+            synchronized (SubChapterDaoIndexOfImpl.class) {
+                if (instance == null) {
+                    instance = new SubChapterDaoIndexOfImpl();
+                }
+            }
+        }
+        return instance;
+    }
+
+    @Override
+    public List<SubChapter> getAll() {
+        return super.getAll();
+    }
+
+    @Override
+    protected SubChapter fromDto(final String dto) {
+        String row = dto;
+        final List<String> elements = new ArrayList<>();
+        String element = row.substring(0, row.indexOf(","));
+        while (true) {
+            elements.add(element);
+            row = row.substring(row.indexOf(",") + 1);
+            if (row.contains(",")) {
+                element = row.substring(0, row.indexOf(","));
+            } else {
+                element = row.substring(0, row.length());
+                elements.add(element);
+                break;
+            }
+
+        }
+        return new SubChapter(elements.get(0), elements.get(1), elements.get(2));
+    }
+
+    @Override
+    public SubChapter getById(final String id) {
+        return super.getById(id);
+    }
+
+    @Override
+    protected String getFileName() {
+        return FILE_NAME;
+    }
 }
