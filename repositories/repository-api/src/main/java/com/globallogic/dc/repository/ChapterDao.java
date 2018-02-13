6adfd05cb5a227800fdc47674948f0e98a99620b
Vitalii Latysh <vitalii.latysh@globallogic.com>
2018-02-13 13:22:09 +0200

    Removed extra method

--- repositories/repository-api/src/main/java/com/globallogic/dc/repository/ChapterDao.java
+++ repositories/repository-api/src/main/java/com/globallogic/dc/repository/ChapterDao.java
@@ -3,6 +3,4 @@ package com.globallogic.dc.repository;
 import com.globallogic.dc.model.Chapter;
 
 public interface ChapterDao extends ProductsDao<Chapter> {
-
-    Chapter getChapterById(String id);
 }
