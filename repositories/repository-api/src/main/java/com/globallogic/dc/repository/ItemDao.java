7fac71bd550e6c9f9865b834e88e06f12e834e6d
Vitalii Latysh <vitalii.latysh@globallogic.com>
2018-02-13 16:59:25 +0200

    Updated getItemsByRangeId()

--- repositories/repository-api/src/main/java/com/globallogic/dc/repository/ItemDao.java
+++ repositories/repository-api/src/main/java/com/globallogic/dc/repository/ItemDao.java
@@ -6,7 +6,5 @@ import java.util.List;
 
 public interface ItemDao extends ProductsDao<Item> {
 
-    List<Item> getItemsByRelatedItemId(String id);
-
     List<Item> getItemsByRangeId(String id);
 }
--- repositories/repository-fs-impl/src/main/java/com/globallogic/dc/repository/fs/impl/ItemDaoImpl.java
+++ repositories/repository-fs-impl/src/main/java/com/globallogic/dc/repository/fs/impl/ItemDaoImpl.java
@@ -1,12 +1,14 @@
 package com.globallogic.dc.repository.fs.impl;
 
 import com.globallogic.dc.model.Item;
-import com.globallogic.dc.repository.ProductsDao;
+import com.globallogic.dc.model.Range;
+import com.globallogic.dc.repository.ItemDao;
 import com.globallogic.dc.repository.fs.AbstractFileSystemDAO;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class ItemDaoImpl extends AbstractFileSystemDAO<Item> implements ProductsDao<Item> {
+public class ItemDaoImpl extends AbstractFileSystemDAO<Item> implements ItemDao {
 
     private static final String FILE_NAME = "items.csv";
     private static volatile ItemDaoImpl instance = null;
@@ -38,11 +40,26 @@ public class ItemDaoImpl extends AbstractFileSystemDAO<Item> implements Products
     protected Item fromDto(final String dto) {
         final String[] fields = dto.split(",");
 
-        return new Item(fields[0], fields[1], fields[2]);
+        final Item result = new Item(fields[0], fields[1], fields[2]);
+        final Range range = new Range();
+
+        range.setKey(fields[3]);
+        result.setRange(range);
+
+        return result;
     }
 
     @Override
     protected String getFileName() {
         return FILE_NAME;
+    }
+
+    @Override
+    public List<Item> getItemsByRangeId(final String id) {
+        return getConnector().readFile(getFile())
+                .stream()
+                .map(this::fromDto)
+                .filter(item -> item.getRange().getKey().equals(id))
+                .collect(Collectors.toList());
     }
 }
