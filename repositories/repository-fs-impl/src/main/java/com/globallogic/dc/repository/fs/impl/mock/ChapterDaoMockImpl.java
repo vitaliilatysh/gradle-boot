1aaa9ea884c73bfeed2dbc82364db1f340a669ae
Vitalii Latysh <vitalii.latysh@globallogic.com>
2018-02-21 19:39:32 +0200

    Add name for Repository bean

--- repositories/repository-fs-impl/src/main/java/com/globallogic/dc/repository/fs/impl/mock/ChapterDaoMockImpl.java
+++ repositories/repository-fs-impl/src/main/java/com/globallogic/dc/repository/fs/impl/mock/ChapterDaoMockImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
+@Repository("ChapterDaoMockImpl")
 public class ChapterDaoMockImpl extends AbstractFileSystemDAO<Chapter> implements ProductsDao<Chapter> {
 
     @Override
