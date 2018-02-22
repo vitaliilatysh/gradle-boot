750e27b61b44b7e49d496ad5583e0bbe40d1990b
Vitalii Latysh <vitalii.latysh@globallogic.com>
2018-02-22 17:48:03 +0200

    Added name for service

--- services/src/main/java/com/globallogic/dc/services/impl/ChapterServiceImpl.java
+++ services/src/main/java/com/globallogic/dc/services/impl/ChapterServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
+@Service("ChapterServiceImpl")
 public class ChapterServiceImpl implements ChapterService {
 
     private final ProductsDao<Chapter> chapterDao;
