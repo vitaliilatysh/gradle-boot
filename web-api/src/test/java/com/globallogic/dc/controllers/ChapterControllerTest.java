a3528acb9c2e41f09b070930a0903e0eb845708b
Vitalii Latysh <vitalii.latysh@globallogic.com>
2018-02-22 12:36:23 +0200

    Added checking all returned objects

--- web-api/src/test/java/com/globallogic/dc/controllers/ChapterControllerTest.java
+++ web-api/src/test/java/com/globallogic/dc/controllers/ChapterControllerTest.java
@@ -11,9 +11,10 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = ChapterController.class)
@@ -36,6 +37,16 @@ public class ChapterControllerTest {
     public void testGetChapters() throws Exception {
         mockMvc.perform(get("/chapters"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE));
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$", hasSize(3)))
+                .andExpect(jsonPath("$[0].key", is("12")))
+                .andExpect(jsonPath("$[0].title", is("Title")))
+                .andExpect(jsonPath("$[0].description", is("Desc")))
+                .andExpect(jsonPath("$[1].key", is("13")))
+                .andExpect(jsonPath("$[1].title", is("Title")))
+                .andExpect(jsonPath("$[1].description", is("Desc")))
+                .andExpect(jsonPath("$[2].key", is("14")))
+                .andExpect(jsonPath("$[2].title", is("Title")))
+                .andExpect(jsonPath("$[2].description", is("Desc")));
     }
 }
