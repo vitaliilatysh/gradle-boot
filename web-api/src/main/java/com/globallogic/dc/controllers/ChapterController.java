c8d8bc3a55d4ac0735caed6266db162ad289e1e6
Vitalii Latysh <vitalii.latysh@globallogic.com>
2018-02-22 17:43:48 +0200

    Updated getChapterById() for controller

--- web-api/src/main/java/com/globallogic/dc/controllers/ChapterController.java
+++ web-api/src/main/java/com/globallogic/dc/controllers/ChapterController.java
@@ -3,8 +3,10 @@ package com.globallogic.dc.controllers;
 import com.globallogic.dc.model.Chapter;
 import com.globallogic.dc.services.impl.ChapterServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,15 +14,17 @@ import java.util.List;
 @RestController
 public class ChapterController {
 
-    private final ChapterServiceImpl chapterService;
-
     @Autowired
-    public ChapterController(ChapterServiceImpl chapterService) {
-        this.chapterService = chapterService;
-    }
+    @Qualifier("ChapterServiceImpl")
+    private ChapterServiceImpl chapterService;
 
     @GetMapping(value = "/chapters", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Chapter> getAllChapters() {
         return chapterService.getChapters();
+    }
+
+    @GetMapping(value = "/chapters/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Chapter getChapterById(@PathVariable("id") final String id) {
+        return chapterService.getChapterById(id);
     }
 }
