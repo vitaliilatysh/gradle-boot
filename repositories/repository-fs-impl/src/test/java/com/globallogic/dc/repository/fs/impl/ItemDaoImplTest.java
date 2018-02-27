1ef6cb5072a51bfac2d242210377cbc702e2fc56
Vitalii Latysh <vitalii.latysh@globallogic.com>
2018-02-27 19:28:18 +0200

    Added @Mock object and sample test method to check mockito

--- repositories/repository-fs-impl/src/test/java/com/globallogic/dc/repository/fs/impl/ItemDaoImplTest.java
+++ repositories/repository-fs-impl/src/test/java/com/globallogic/dc/repository/fs/impl/ItemDaoImplTest.java
@@ -1,24 +1,43 @@
 package com.globallogic.dc.repository.fs.impl;
 
+import com.globallogic.dc.connector.FileSystemConnectorImpl;
 import com.globallogic.dc.model.Item;
 import com.globallogic.dc.repository.ItemDao;
 import com.globallogic.dc.repository.fs.config.RepositoryConfig;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(MockitoJUnitRunner.class)
 @ContextConfiguration(classes = RepositoryConfig.class)
 public class ItemDaoImplTest {
 
-    @Autowired
+    @Mock
+    private FileSystemConnectorImpl fileSystemConnector;
+
+    @Mock
     private ItemDao itemDao;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+
+    }
+
+    @Test
+    public void testMockCreation() {
+        assertNotNull(fileSystemConnector);
+        assertNotNull(itemDao);
+    }
 
     @Test
     public void testGetItemsByRangeId() {
