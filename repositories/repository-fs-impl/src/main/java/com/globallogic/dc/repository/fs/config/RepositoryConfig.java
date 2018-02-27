69f23291d5bc704cc1d68147a0afe492fcb97d32
Vitalii Latysh <vitalii.latysh@globallogic.com>
2018-02-27 19:01:17 +0200

    Made ComponentScan only for package inside

--- repositories/repository-fs-impl/src/main/java/com/globallogic/dc/repository/fs/config/RepositoryConfig.java
+++ repositories/repository-fs-impl/src/main/java/com/globallogic/dc/repository/fs/config/RepositoryConfig.java
@@ -2,8 +2,6 @@ package com.globallogic.dc.repository.fs.config;
 
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = {
-        "com.globallogic.dc.connector",
-        "com.globallogic.dc.repository.fs"})
+@ComponentScan(basePackages = "com.globallogic.dc.repository.fs")
 public class RepositoryConfig {
 }
