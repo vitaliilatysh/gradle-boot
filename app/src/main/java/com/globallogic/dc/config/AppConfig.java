ee942fbdd06dcf474c23f68a6237e8409a7791e8
Vitalii Latysh <vitalii.latysh@globallogic.com>
2018-03-05 18:48:12 +0200

    Added Spring Boot

--- app/src/main/java/com/globallogic/dc/config/AppConfig.java
+++ app/src/main/java/com/globallogic/dc/config/AppConfig.java
@@ -1,9 +1,13 @@
 package com.globallogic.dc.config;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@SpringBootConfiguration
 @ComponentScan(basePackages = "com.globallogic.dc.config")
 public class AppConfig {
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfig.class, args);
+    }
 }
