5666f763599ec8ae409e913fb920a99a6d470860
Vitalii Latysh <vitalii.latysh@globallogic.com>
2018-03-06 19:33:29 +0200

    Added config packages for ComponentScan

--- app/src/main/java/com/globallogic/dc/App.java
+++ app/src/main/java/com/globallogic/dc/App.java
@@ -1,9 +1,18 @@
 package com.globallogic.dc;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootConfiguration
+@ComponentScan(basePackages = {
+        "com.globallogic.dc.connector",
+        "com.globallogic.dc.repository.fs.config",
+        "com.globallogic.dc.service.config",
+        "com.globallogic.dc.controllers.config"
+})
+@EnableAutoConfiguration
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
