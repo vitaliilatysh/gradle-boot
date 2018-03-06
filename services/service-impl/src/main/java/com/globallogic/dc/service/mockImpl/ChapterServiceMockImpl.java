76bd825264a2aa5ed603491f26a63d6fb19156bc
Vitalii Latysh <vitalii.latysh@globallogic.com>
2018-03-06 19:25:22 +0200

    Reverted back mock for ChapterServiceMockImpl

--- services/service-impl/src/main/java/com/globallogic/dc/service/mockImpl/ChapterServiceMockImpl.java
+++ services/service-impl/src/main/java/com/globallogic/dc/service/mockImpl/ChapterServiceMockImpl.java
@@ -1,8 +1,24 @@
 package com.globallogic.dc.service.mockImpl;
 
+import com.globallogic.dc.model.Chapter;
+import com.globallogic.dc.service.ChapterService;
 import org.springframework.stereotype.Service;
 
-@Service
-public class ChapterServiceMockImpl {
+import java.util.Arrays;
+import java.util.List;
 
+@Service
+public class ChapterServiceMockImpl implements ChapterService {
+
+    public List<Chapter> getChapters() {
+        return Arrays.asList(
+                new Chapter("1", "Title", "Desc"),
+                new Chapter("2", "Title", "Desc"));
+    }
+
+    public Chapter getChapterById(final String key) {
+        Chapter chapter = new Chapter("1", "Title", "Desc");
+
+        return chapter.getKey().equals(key) ? chapter : new Chapter();
+    }
 }
