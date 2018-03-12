3d5f618277d3a829df64f5d13c215a75035477de
Vitalii Latysh <vitalii.latysh@globallogic.com>
2018-03-12 18:07:51 +0200

    Added RequestHandlerSelectors by RestController, updated PathSelectors with regex

--- swagger-api/src/main/java/com/globallogic/dc/config/SwaggerConfig.java
+++ swagger-api/src/main/java/com/globallogic/dc/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.globallogic.dc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -16,8 +17,8 @@ public class SwaggerConfig {
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .select().apis(RequestHandlerSelectors.basePackage("com.globallogic.dc.web.api"))
-                .paths(PathSelectors.any())
+                .select().apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.regex("/.*"))
                 .build()
                 .apiInfo(apiInfo());
     }
