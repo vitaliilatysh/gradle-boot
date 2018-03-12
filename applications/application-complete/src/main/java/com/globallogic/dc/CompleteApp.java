e342230705dcbc9193752e6ce5136d6a6a858e9e
Vitalii Latysh <vitalii.latysh@globallogic.com>
2018-03-12 20:00:33 +0200

    Renamed the apps

--- applications/application-complete/src/main/java/com/globallogic/dc/CompleteApp.java
+++ applications/application-complete/src/main/java/com/globallogic/dc/CompleteApp.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.globallogic.dc.config")
-public class App {
+public class CompleteApp {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(CompleteApp.class, args);
     }
 }
